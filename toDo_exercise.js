0cac1e3d0b22614eac842f372aa26dbe6cc068ee
liberala425 <liberala425@gmail.com>
2024-01-03 15:47:14 -0800

    rename js file and variables inside

---
+++ toDo_exercise.js
@@ -0,0 +1,35 @@
+const todoform = document.querySelector("#todoform");
+const todoList = document.querySelector('#results');
+const todoInput = document.querySelector('input[name="todo"]');
+const currenTodDoList = [];
+
+todoform.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const newToDo = document.createElement('li');
+    newToDo.innerText = todoInput.value;
+    newToDo.classList.add('todo');
+    todoInput.value = '';
+    const completeBtn = document.createElement('button');
+    completeBtn.innerText = 'Complete';
+
+    const removeBtn = document.createElement('button');
+    removeBtn.innerText = 'Remove';
+    newToDo.append(completeBtn);
+    newToDo.append(removeBtn)
+    todoList.append(newToDo);
+    currenTodDoList.push(newToDo);
+    localStorage.setItem("currenTodDoList", JSON.stringify(currenTodDoList));
+})
+
+
+todoList.addEventListener('click', function(e){
+    if(e.target.tagName === 'BUTTON'){
+        if(e.target.innerText === 'Complete') {
+            e.target.parentElement.classList.add('completed');
+            e.target.remove(); //remove the "Complete button"
+        } else if (e.target.innerText === 'Remove') {
+            e.target.parentElement.remove();
+        }
+    }
+})
