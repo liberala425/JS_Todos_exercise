53cfa5d1bd527f0a8a5ab4a206913cd864945c96
liberala425 <liberala425@gmail.com>
2024-01-04 13:51:30 -0800

    fixed errors when refreshing the page

--- toDoLocalStorage.js
+++ toDoLocalStorage.js
@@ -3,20 +3,25 @@ const todoList = document.querySelector('#results');
 const todoInput = document.querySelector('input[name="todo"]');
 const currenTodDoList = JSON.parse(localStorage.getItem("todo")) || [];
 
-if(currenTodDoList !== null){
+if(currenTodDoList.length !== 0){
     for(let i=0; i<currenTodDoList.length; i++){
         const toDoItem = document.createElement("li");
-        toDoItem.innerText = currenTodDoList[i].toDo;
+        const ToDoText = document.createElement('span');
+        ToDoText.innerText = currenTodDoList[i].val;
         toDoItem.classList.add('todo');
+        toDoItem.append(ToDoText);
         if (currenTodDoList[i].Completed){
             toDoItem.classList.add('completed');
+        } else{
+            const completeBtn = document.createElement('button');
+            completeBtn.innerText = 'Complete';
+            toDoItem.append(completeBtn);
         }
-        const completeBtn = document.createElement('button');
-        completeBtn.innerText = 'Complete';
 
         const removeBtn = document.createElement('button');
         removeBtn.innerText = 'Remove';
-        toDoItem.append(completeBtn);
+        
+        
         toDoItem.append(removeBtn)
         todoList.append(toDoItem);
     }
@@ -25,32 +30,50 @@ todoform.addEventListener('submit', function(e){
     e.preventDefault();
 
     const newToDo = document.createElement('li');
-    newToDo.innerText = todoInput.value;
+    const newToDoText = document.createElement('span');
+    newToDoText.innerText = todoInput.value;
     newToDo.classList.add('todo');
     todoInput.value = '';
     newToDo.Completed = false;
-    currenTodDoList.push({toDo:newToDo.innerText, Completed: false});
+    currenTodDoList.push({val:newToDoText.innerText, Completed: false});
     localStorage.setItem("todo", JSON.stringify(currenTodDoList));
     const completeBtn = document.createElement('button');
     completeBtn.innerText = 'Complete';
 
     const removeBtn = document.createElement('button');
     removeBtn.innerText = 'Remove';
+    newToDo.append(newToDoText);
     newToDo.append(completeBtn);
     newToDo.append(removeBtn)
     todoList.append(newToDo);
 
 })
 
-
 todoList.addEventListener('click', function(e){
+    const parsedList = JSON.parse(localStorage.todo);
     if(e.target.tagName === 'BUTTON'){
         if(e.target.innerText === 'Complete') {
             e.target.parentElement.classList.add('completed');
-            e.target.remove(); //remove the "Complete button"
+            
             e.target.Completed = true;
+            for(let i=0; i<parsedList.length; i++){
+                if(e.target.parentElement.firstChild.innerText == parsedList[i]['val']){
+                    parsedList[i]['Completed'] = true;
+                }
+            }
+            e.target.remove(); //remove the "Complete button"
         } else if (e.target.innerText === 'Remove') {
-            e.target.parentElement.remove();
+            console.log(e.target.parentElement);
+            for(let i=0; i<parsedList.length; i++){  
+                if(e.target.parentElement.firstChild.innerText == parsedList[i]['val']){
+                    parsedList.splice(i, 1);
+                    //console.log(parsedList);
+                    e.target.parentElement.remove();
+                }
+            }
         }
+    localStorage.removeItem('todo');
+    localStorage.setItem("todo", JSON.stringify(parsedList));
     }
 })
+
